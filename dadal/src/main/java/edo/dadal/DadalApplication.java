097cc2b7e5ba05c215f4ad563a36d3052e32eacd
이다영 <yooung513@gmail.com>
2023-12-13 20:23:39 +0900

    231213 init

---
+++ dadal/src/main/java/edo/dadal/DadalApplication.java
@@ -0,0 +1,13 @@
+package edo.dadal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DadalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DadalApplication.class, args);
+	}
+
+}
---
+++ dadal/src/test/java/edo/dadal/DadalApplicationTests.java
@@ -0,0 +1,13 @@
+package edo.dadal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DadalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
